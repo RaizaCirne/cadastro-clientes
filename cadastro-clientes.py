07fc107c9e00c5a62c4f305cfc138d6f6314e9fc
RaizaCirne <raizabraz93@gmail.com>
2022-10-22 14:40:11 -0300

    atualizar projeto

--- cadastro-clientes.py
+++ cadastro-clientes.py
@@ -1,9 +1,20 @@
 # Este programa pede ao usuário os dados pessoais de um cliente.
 # Armazena os dados na memória RAM e exibe na tela os dados dos clientes cadastrados
 
+from datetime import datetime
+
 nome = input('Digite o nome do cliente: ')
-datanasc = input('Digite a data de nascimento: ')
+
+anonasc = int(input('Digite o ano de nascimento: '))
+mesnasc = int(input('Digite o mês do nascimento: '))
+dianasc = int(input('Digite o dia do nascimento: '))
+
+
+datanasc = datetime(anonasc, mesnasc, dianasc)
+dataatual = datetime.now()
+idadecliente = dataatual - datanasc
+
 endereco = input('Digite o endereço: ')
 tel = input('Digite o telefone: ')
 
-print('Cliente: {}\nData de nascimento {}\nTelefone: {}'.format(nome, datanasc, endereco, tel))
+print('Cliente: {}\nIdade do cliente {}\nTelefone: {}'.format(nome, idadecliente, endereco, tel))
